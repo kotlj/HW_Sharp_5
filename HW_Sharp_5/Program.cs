e2a6ddce07125586429aa5bf289cbd67cfd2fc36
kotlj <crotlj6@gmail.com>
2023-07-12 19:53:39 +0300

    done

---
+++ HW_Sharp_5/Program.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Sharp_5
+{
+    internal class Program
+    {
+
+        class WebSite
+        {
+            private string name;
+            private string url;
+            private string description;
+            private string ip;
+            
+            public string Name
+            {
+                get
+                {
+                    return name;
+                }
+                set
+                {
+                    name = value;
+                }
+            }
+            public string Url
+            {
+                get
+                {
+                    return url;
+                }
+                set
+                {
+                    url = value; 
+                }
+            }
+            public string Description
+            {
+                get
+                {
+                    return description;
+                }
+                set
+                {
+                    description = value;
+                }
+            }
+            public string Ip
+            {
+                get
+                {
+                    return ip;
+                }
+                set
+                {
+                    ip = value;
+                }
+            }
+        }
+        class Magazine
+        {
+            private string name;
+            private string year;
+            private string description;
+            private string phone;
+            private string email;
+
+            public string Name
+            {
+                get
+                {
+                    return name;
+                }
+                set
+                {
+                    name = value;
+                }
+            }
+            public string Year
+            {
+                get
+                {
+                    return year;
+                }
+                set
+                {
+                    year = value;
+                }
+            }
+            public string Description
+            {
+                get
+                {
+                    return description;
+                }
+                set
+                {
+                    description = value;
+                }
+            }
+            public string Phone
+            {
+                get
+                {
+                    return phone;
+                }
+                set
+                {
+                    phone = value;
+                }
+            }
+            public string Email
+            {
+                get
+                {
+                    return email;
+                }
+                set
+                {
+                    email = value;
+                }
+            }
+        }
+        class Shop
+        {
+            private string name;
+            private string address;
+            private string description;
+            private string phone;
+            private string email;
+
+            public string Name
+            {
+                get
+                {
+                    return name;
+                }
+                set
+                {
+                    name = value;
+                }
+            }
+            public string Address
+            {
+                get
+                {
+                    return address;
+                }
+                set
+                {
+                    address = value;
+                }
+            }
+            public string Description
+            {
+                get
+                {
+                    return description;
+                }
+                set
+                {
+                    description = value;
+                }
+            }
+            public string Phone
+            {
+                get
+                {
+                    return Phone;
+                }
+                set
+                {
+                    phone= value;
+                }
+            }
+            public string Email
+            {
+                get
+                {
+                    return email;
+                }
+                set
+                {
+                    email = value;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
